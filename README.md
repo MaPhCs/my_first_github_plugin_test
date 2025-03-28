203657e8948df53017edd1fe5198672b5422bbdb
MaPhCs <qwerasdfw@sogang.ac.kr>
2025-03-29 03:22:22 +0900

    1st_commit

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # my_first_github_plugin_test
 this is my_first_github_plugin_test
+
+The important thing may be good.
+sdsd
--- test220213/test220213/test220213.cpp
+++
@@ -1,9 +0,0 @@
-﻿#include <iostream>
-
-int main()
-{
-    std::cout << "Hello World!\n";
-}
-
-// push를 하기 위해서는 개인이 build를 하여 정상작동이 되는 것을 확인하고 최종적으로
-// 실행할 것.
